467d4697e75fd06e9aadafdd335fee313fc7c78c
3draphicsplus.com <3dgraphicsplus.com>
2019-11-27 16:49:24 +0700

    Add image display and switch image

--- Test_img.py
+++ Test_img.py
@@ -89,47 +89,62 @@ def test(imgL,imgR):
 
 def main():
        processed = preprocess.get_transform(augment=False)
-       if args.isgray:
-           imgL_o = cv2.cvtColor(cv2.imread(args.leftimg,0), cv2.COLOR_GRAY2RGB)
-           imgR_o = cv2.cvtColor(cv2.imread(args.rightimg,0), cv2.COLOR_GRAY2RGB)
-       else:
-           imgL_o = (skimage.io.imread(args.leftimg).astype('float32'))
-           imgR_o = (skimage.io.imread(args.rightimg).astype('float32'))
-       
-       imgL = processed(imgL_o).numpy()
-       imgR = processed(imgR_o).numpy()
-       imgL = np.reshape(imgL,[1,3,imgL.shape[1],imgL.shape[2]])
-       imgR = np.reshape(imgR,[1,3,imgR.shape[1],imgR.shape[2]])
+       index = -1;
+       while True:
+        choice = raw_input("> ")
+        choice = choice.lower() #Convert input to "lowercase"
 
-       # pad to width and hight to 16 times
-       if imgL.shape[2] % 16 != 0:
-            times = imgL.shape[2]//16       
-            top_pad = (times+1)*16 -imgL.shape[2]
-       else:
-            top_pad = 0
-       if imgL.shape[3] % 16 != 0:
-            times = imgL.shape[3]//16                       
-            left_pad = (times+1)*16-imgL.shape[3]
-       else:
-            left_pad = 0     
-       imgL = np.lib.pad(imgL,((0,0),(0,0),(top_pad,0),(0,left_pad)),mode='constant',constant_values=0)
-       imgR = np.lib.pad(imgR,((0,0),(0,0),(top_pad,0),(0,left_pad)),mode='constant',constant_values=0)
+        if choice == 'exit':
+            print("Good bye.")
+            return
 
-       start_time = time.time()
-       pred_disp = test(imgL,imgR)
-       print('time = %.2f' %(time.time() - start_time))
-       if top_pad !=0 or left_pad != 0:
-            img = pred_disp[top_pad:,:-left_pad]
-       else:
-            img = pred_disp
-       img = (img*256).astype('uint16')
-       skimage.io.imsave('disparity.png',img)
-       
-       #img = np.concatenate((imgL_o, imgR_o),axis=1)
-       #img = cv2.line(img, (0, 240), (1504, 240), (0, 0, 255), 2)
-       #img = cv2.line(img, (0, 210), (1504, 210), (0, 0, 255), 2)
-       #img = cv2.line(img, (0, 270), (1504, 270), (0, 0, 255), 2)
-       #skimage.io.imsave('test.png',img)
+        if choice == 'n':
+          print("Next image "+ str(index))
+          index = index + 1
+          if args.isgray:
+             imgL_o = cv2.cvtColor(cv2.imread(args.leftimg+str(index).zfill(6)+"_10.png",0), cv2.COLOR_GRAY2RGB)
+             imgR_o = cv2.cvtColor(cv2.imread(args.rightimg+str(index).zfill(6)+"_11.png",0), cv2.COLOR_GRAY2RGB)
+          else:
+             imgL_o = (skimage.io.imread(args.leftimg+str(index).zfill(6)+"_10.png").astype('float32'))
+             imgR_o = (skimage.io.imread(args.rightimg+str(index).zfill(6)+"_11.png").astype('float32'))
+
+          imgL = processed(imgL_o).numpy()
+          imgR = processed(imgR_o).numpy()
+          imgL = np.reshape(imgL,[1,3,imgL.shape[1],imgL.shape[2]])
+          imgR = np.reshape(imgR,[1,3,imgR.shape[1],imgR.shape[2]])
+
+          # pad to width and hight to 16 times
+          if imgL.shape[2] % 16 != 0:
+              times = imgL.shape[2]//16       
+              top_pad = (times+1)*16 -imgL.shape[2]
+          else:
+              top_pad = 0
+          if imgL.shape[3] % 16 != 0:
+              times = imgL.shape[3]//16                       
+              left_pad = (times+1)*16-imgL.shape[3]
+          else:
+              left_pad = 0     
+          imgL = np.lib.pad(imgL,((0,0),(0,0),(top_pad,0),(0,left_pad)),mode='constant',constant_values=0)
+          imgR = np.lib.pad(imgR,((0,0),(0,0),(top_pad,0),(0,left_pad)),mode='constant',constant_values=0)
+
+          start_time = time.time()
+          pred_disp = test(imgL,imgR)
+          print('time = %.2f' %(time.time() - start_time))
+          if top_pad !=0 or left_pad != 0:
+              img = pred_disp[top_pad:,:-left_pad]
+          else:
+              img = pred_disp
+          img = (img*1).astype('uint8')
+          skimage.io.imsave('disparity.png',img)
+          im_color = cv2.applyColorMap(img, cv2.COLORMAP_JET)
+          cv2.imshow('imageLeft',imgL_o)
+          cv2.imshow('depth',im_color)
+          cv2.waitKey(0)
+          #img = np.concatenate((imgL_o, imgR_o),axis=1)
+          #img = cv2.line(img, (0, 240), (1504, 240), (0, 0, 255), 2)
+          #img = cv2.line(img, (0, 210), (1504, 210), (0, 0, 255), 2)
+          #img = cv2.line(img, (0, 270), (1504, 270), (0, 0, 255), 2)
+          #skimage.io.imsave('test.png',img)
 
 if __name__ == '__main__':
    main()
